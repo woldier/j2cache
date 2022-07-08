dd0c4cbe399bef4e6b52f893195319a944fb2ce1
woldier <1098582358@qq.com>
2022-07-08 19:30:22 +0800

    j2cache的使用

---
+++ src/main/java/com/woldier/controller/J2CacheApplication.java
@@ -0,0 +1,11 @@
+package com.woldier.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2CacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(J2CacheApplication.class,args);
+    }
+}
